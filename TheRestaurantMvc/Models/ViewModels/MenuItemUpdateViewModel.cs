4e1bf13c9b63af744df580c9a00bce7bf2c07c01
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-09-10 18:43:57 +0200

    Fixed validation for create and update

--- TheRestaurantMvc/Models/ViewModels/MenuItemUpdateViewModel.cs
+++ TheRestaurantMvc/Models/ViewModels/MenuItemUpdateViewModel.cs
@@ -1,9 +1,11 @@
-﻿namespace TheRestaurantMvc.Models.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheRestaurantMvc.Models.ViewModels;
 
 public record MenuItemUpdateViewModel(
-    string Name,
-    decimal Price,
-    string Description,
-    string Image,
-    bool IsPopular 
+    [Required(ErrorMessage = "Name is required.")] string Name,
+    [Required(ErrorMessage = "Price is required.")][Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")] decimal Price,
+    [Required(ErrorMessage = "Description is required.")] string Description,
+    [Required(ErrorMessage = "Image URL is required.")] string Image,
+    bool IsPopular = false
     );
