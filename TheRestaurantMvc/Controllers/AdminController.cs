3ebf803b3ab64bd86d58803c7e3194142e3769f4
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-09-10 17:13:50 +0200

    Can now create menu item

--- TheRestaurantMvc/Controllers/AdminController.cs
+++ TheRestaurantMvc/Controllers/AdminController.cs
@@ -12,14 +12,20 @@ public class AdminController(
     IRestaurantApiClient client) : Controller
 {
     public async Task<IActionResult> Menu() => View(await service.GetMenuItemsAsync());
-
-    public async Task<IActionResult> EditMenuItem(int id) => View(await service.GetMenuItemByIdAsync(id));
-
-    public async Task<IActionResult> Update(int id, MenuItemUpdateViewModel model)
+    
+    public IActionResult CreateMenuItem() => View();
+    [HttpPost]
+    public async Task<IActionResult> CreateMenuItem(MenuItemCreateViewModel viewModel)
     {
-        await client.TheRestaurantApiClient()
-            .PatchAsJsonAsync($"menu-items/{id}", model);
-        
+        await client.TheRestaurantApiClient().PostAsJsonAsync("menu-items", viewModel);
+        return RedirectToAction("Menu", "Admin");
+    }
+    
+    public async Task<IActionResult> UpdateMenuItem(int id) => View(await service.GetMenuItemByIdAsync(id));
+    [HttpPost]
+    public async Task<IActionResult> UpdateMenuItem(int id, MenuItemUpdateViewModel model)
+    {
+        await client.TheRestaurantApiClient().PatchAsJsonAsync($"menu-items/{id}", model);
         return RedirectToAction("Menu", "Admin");
     }
 }
---
+++ TheRestaurantMvc/Models/ViewModels/MenuItemCreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheRestaurantMvc.Models.ViewModels;
+
+public record MenuItemCreateViewModel(
+    [Required(ErrorMessage = "Name is required.")] string Name,
+    [Required(ErrorMessage = "Price is required.")][Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")] decimal Price,
+    [Required(ErrorMessage = "Description is required.")] string Description,
+    [Required(ErrorMessage = "Image URL is required.")] string Image,
+    bool IsPopular = false
+);
