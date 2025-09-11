866eddfe276c7aa07ad65149e9034e2e79139f96
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-09-11 16:51:49 +0200

    Fixed file formatting and cleaned up files

--- TheRestaurantMvc/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TheRestaurantMvc.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
